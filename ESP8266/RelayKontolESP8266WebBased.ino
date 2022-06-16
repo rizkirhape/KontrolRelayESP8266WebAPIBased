d859e8aa63403ec3b5054579fe6d027cfaa1c275
Rizki Heri Purwanto <106834486+rizkirhape@users.noreply.github.com>
2022-06-16 07:01:55 +0700

    Update RelayKontolESP8266WebBased.ino

--- ESP8266/RelayKontolESP8266WebBased.ino
+++ ESP8266/RelayKontolESP8266WebBased.ino
@@ -259,7 +259,7 @@ void loop() {
 //===================================================End-Of-Void-Loop================================================================
 //---------------------------------------------------------------------------------
 //Project: Kontrol Relay Berbasis Web API Request, LCD Monitor, Dan WiFiManager   |
-//Author: Anjas Amar Pradana                                                      |
+//Author: Rizki Heri Purwanto                                                   |
 //Mail: atsidev.io@gmail.com                                                      |
 //Program khusus board: esp8266 (Nodemcu)                                         |
 //Official Web API Service: http://mpsri.atwebpages.com                           |
